d9c4c78696def75f9dc21c809490c6d13844660f
sabda21 <106018056+sabda21@users.noreply.github.com>
2022-07-21 08:55:03 +0700

    Add files via upload

--- userbot/modules/evaluators.py
+++ userbot/modules/evaluators.py
@@ -185,12 +185,194 @@ async def terminal_runner(term):
     else:
         await term.edit(f"`{curruser}:~$ {command}\n{result}`")
 
+@register(outgoing=True, pattern=r"^\.r(?: |$|\n)([\s\S]*)")
+async def _exec_term(message):
+    """ run commands in shell (terminal with live update) """
+    await message.edit("`Executing terminal ...`")
+    cmd = message.pattern_match.group(1)
+
+    try:
+        parsed_cmd = cmd
+    except Exception as e:  # pylint: disable=broad-except
+        await message.edit(str(e))
+        return
+    try:
+        t_obj = await Terminal.execute(parsed_cmd)  # type: Term
+    except Exception as t_e:  # pylint: disable=broad-except
+        await message.edit(str(t_e))
+        return
+
+    cur_user = getuser()
+    uid = geteuid()
+
+    prefix = f"**{cur_user}:~#**" if uid == 0 else f"**{cur_user}:~$**"
+    output = f"{prefix} `{cmd}`\n"
+
+    count = 0
+    k = None
+    while not t_obj.finished:
+        count += 1
+        await asyncio.sleep(0.3)
+        if count >= 5:
+            count = 0
+            out_data = f"{prefix}`{t_obj.line}`"
+            try:
+                if not k:
+                    k = await message.edit(out_data)
+                else:
+                    await k.edit(out_data)
+            except Exception:
+                pass
+
+    out_data = f"{output}`{t_obj.output}`\n"
+    await t_obj.wait(3)
+
+    if len(out_data) > 4096:
+        output = open("terminal.txt", "w+")
+        output.write(out_data)
+        output.close()
+        await message.client.send_file(
+            message.chat_id,
+            "terminal.txt",
+            reply_to=message.id,
+            caption="`Output too large, sending as file`",
+        )
+        remove("terminal.txt")
+        return
+    else:
+        await message.edit(out_data)
+    del out_data
 
 CMD_HELP.update(
     {
         "eval": ">`.eval print('world')`" "\nUsage: Just like exec.",
         "exec": ">`.exec print('hello')`" "\nUsage: Execute small python scripts.",
-        "term": ">`.term <cmd>`"
+        "term": ">`.term <cmd>`",
+        "run": ">`.r <cmd>`"
         "\nUsage: Run bash commands and scripts on your server.",
     }
 )
+
+try:
+    from os import geteuid, setsid, getpgid, killpg
+    from signal import SIGKILL
+except ImportError:
+    # pylint: disable=ungrouped-imports
+    from os import kill as killpg
+    from signal import CTRL_C_EVENT as SIGKILL
+
+    def geteuid() -> int:
+        return 1
+
+    def getpgid(arg: Any) -> Any:
+        return arg
+
+    setsid = None
+
+class Terminal:
+    """ live update term class """
+
+    def __init__(self, process: asyncio.subprocess.Process) -> None:
+        self._process = process
+        self._line = b''
+        self._output = b''
+        self._init = asyncio.Event()
+        self._is_init = False
+        self._cancelled = False
+        self._finished = False
+        self._loop = asyncio.get_running_loop()
+        self._listener = self._loop.create_future()
+
+    @property
+    def line(self) -> str:
+        return self._by_to_str(self._line)
+    
+    @property
+    def output(self) -> str:
+        return self._by_to_str(self._output)
+
+    @staticmethod
+    def _by_to_str(data: bytes) -> str:
+        return data.decode('utf-8', 'replace').rstrip()
+
+    @property
+    def cancelled(self) -> bool:
+        return self._cancelled
+
+    @property
+    def finished(self) -> bool:
+        return self._finished
+
+    async def init(self) -> None:
+        await self._init.wait()
+
+    async def wait(self, timeout: int) -> None:
+        self._check_listener()
+        try:
+            await asyncio.wait_for(self._listener, timeout)
+        except asyncio.TimeoutError:
+            pass
+
+    def _check_listener(self) -> None:
+        if self._listener.done():
+            self._listener = self._loop.create_future()
+
+    def cancel(self) -> None:
+        if self._cancelled or self._finished:
+            return
+        killpg(getpgid(self._process.pid), SIGKILL)
+        self._cancelled = True
+
+    @classmethod
+    async def execute(cls, cmd: str) -> 'Terminal':
+        kwargs = dict(
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE)
+        if setsid:
+            kwargs['preexec_fn'] = setsid
+        process = await asyncio.create_subprocess_shell(cmd, **kwargs)
+        t_obj = cls(process)
+        t_obj._start()
+        return t_obj
+
+    def _start(self) -> None:
+        self._loop.create_task(self._worker())
+
+    async def _worker(self) -> None:
+        if self._cancelled or self._finished:
+            return
+        await asyncio.wait([self._read_stdout(), self._read_stderr()])
+        await self._process.wait()
+        self._finish()
+
+    async def _read_stdout(self) -> None:
+        await self._read(self._process.stdout)
+
+    async def _read_stderr(self) -> None:
+        await self._read(self._process.stderr)
+
+    async def _read(self, reader: asyncio.StreamReader) -> None:
+        while True:
+            line = await reader.read(n=1024)
+            if not line:
+                break
+            self._append(line)
+
+    def _append(self, line: bytes) -> None:
+        self._line = line
+        self._output += line
+        self._check_init()
+
+    def _check_init(self) -> None:
+        if self._is_init:
+            return
+        self._loop.call_later(1, self._init.set)
+        self._is_init = True
+
+    def _finish(self) -> None:
+        if self._finished:
+            return
+        self._init.set()
+        self._finished = True
+        if not self._listener.done():
+            self._listener.set_result(None)
